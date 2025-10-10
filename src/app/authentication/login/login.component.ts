3e31a1bcdc949ed35f208a12b7437c31c180b177
Fouy <mannyivfouy@gmail.com>
2025-10-10 19:39:35 +0700

    Login[F

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrl: './login.component.css',    
 })
-export class LoginComponent {}
+export class LoginComponent {
+  
+}
